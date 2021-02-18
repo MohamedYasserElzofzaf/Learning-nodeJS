5a33c90a682b8599cce9554ca6559499ea378878
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-02-18 21:11:54 +0200

    create server

--- Server.js
+++ Server.js
@@ -6,24 +6,34 @@ const server = http.createServer((req, res) => {
     const method = req.method;
     if (url === "/") {
         res.write("<html>");
-        res.write("<head><title>Enter the message</title> </head>");
+        res.write("<head><title>Enter Message</title><head>");
         res.write(
-            '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit" >SEND</button></form></body>'
+            '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
         );
         res.write("</html>");
         return res.end();
     }
     if (url === "/message" && method === "POST") {
-        fs.writeFileSync("message.txt", "DUMMY");
+        const body = [];
+        req.on("data", (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            fs.writeFileSync("message.txt", message);
+        });
         res.statusCode = 302;
         res.setHeader("Location", "/");
         return res.end();
     }
-    res.setHeader("content-Type", "text/html");
+    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
-    res.write("<head><title>My page</title> </head>");
-    res.write("<body><h1>Hello World!!!</h1></body>");
+    res.write("<head><title>My First Page</title><head>");
+    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
     res.write("</html>");
     res.end();
 });
+
 server.listen(3000);

f6327c5be136023d8b72ecd51f4455011cd4bd9d
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-02-21 02:00:15 +0200

    handling deferent routes

--- Server.js
+++ Server.js
@@ -1,7 +1,13 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
-const server = http.createServer(app);
-
-server.listen(3000);
+app.use("/add", (req, res, next) => {
+    console.log("in the middleware!");
+    res.send("<h1>Hello is it me you looking for!!!</h1>");
+    next();
+});
+app.use("/", (req, res, next) => {
+    console.log("in another middleware!");
+    res.send("<h1>Are you somewhere feeling lonely?!</h1>");
+});
+app.listen(3000);

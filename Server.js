306030085ae032e0b500c4435908590dc2ef66ab
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-02-22 20:06:02 +0200

    Working with Handlebars

--- Server.js
+++ Server.js
@@ -5,9 +5,16 @@ const bodyParser = require("body-parser");
 const expressHbs = require("express-handlebars");
 
 const app = express();
-app.engine("hbs", expressHbs());
+
+app.engine(
+    "hbs",
+    expressHbs({
+        layoutsDir: "views/layouts/",
+        defaultLayout: "main-layout",
+        extname: "hbs",
+    })
+);
 app.set("view engine", "hbs");
-// app.set("view engine", "pug");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -20,7 +27,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render("404", { pageTitle: "Page Not Found!" });
+    res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -11,9 +11,11 @@ router.get("/", (req, res, next) => {
     const products = adminData.products;
     res.render("shop", {
         prods: products,
-        pageTitle: "shop",
-        path: "/shop",
-        hasProduct: products.length > 0,
+        pageTitle: "Shop",
+        path: "/",
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true,
     });
 });
 

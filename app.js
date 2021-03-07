12a9370a919719cd338727143d5b9402acd42fa3
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-03-07 15:10:54 +0200

    Working with mongoose && saving Data through mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 
-const User = require("./models/user");
+//const User = require("./models/user");
 const mongoose = require("mongoose");
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-    User.findById("5baa2528563f16379fc8a610")
-        .then((user) => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
-            next();
-        })
-        .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//     User.findById("5baa2528563f16379fc8a610")
+//         .then((user) => {
+//             req.user = new User(user.name, user.email, user.cart, user._id);
+//             next();
+//         })
+//         .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -34,7 +34,7 @@ app.use(errorController.get404);
 
 mongoose
     .connect(
-        "mongodb+srv://Mohamed9Yasser:qxRcvxU5eCXPfi2H@cluster0.vtzp8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        "mongodb+srv://Mohamed9Yasser:qxRcvxU5eCXPfi2H@cluster0.vtzp8.mongodb.net/Shop?retryWrites=true&w=majority"
     )
     .then((result) => {
         app.listen(3000);

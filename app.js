e1f95ad5242dadaa7b5ce1b4f4ce5b501712b99c
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-03-10 00:48:15 +0200

    install session package to the project && connect it with mongoDB

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -14,9 +15,13 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+    session({ secret: "my secret", resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
     User.findById("6045dfac08967b410453f2e2")

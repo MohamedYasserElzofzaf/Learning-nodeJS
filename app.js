d350434da27bf93e0c186a368299b891ae76a69a
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-03-10 01:12:38 +0200

    use mongoDB to store sessions

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
+const MONGODB_URI =
+    "mongodb+srv://Mohamed9Yasser:qxRcvxU5eCXPfi2H@cluster0.vtzp8.mongodb.net/Shop";
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -20,7 +28,12 @@ const authRoutes = require("./routes/auth");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-    session({ secret: "my secret", resave: false, saveUninitialized: false })
+    session({
+        secret: "my secret",
+        resave: false,
+        saveUninitialized: false,
+        store: store,
+    })
 );
 
 app.use((req, res, next) => {
@@ -39,15 +52,13 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-    .connect(
-        "mongodb+srv://Mohamed9Yasser:qxRcvxU5eCXPfi2H@cluster0.vtzp8.mongodb.net/Shop?retryWrites=true&w=majority"
-    )
+    .connect(MONGODB_URI)
     .then((result) => {
         User.findOne().then((user) => {
             if (!user) {
                 const user = new User({
-                    name: "Yasser",
-                    email: "yasser@test.com",
+                    name: "Max",
+                    email: "max@test.com",
                     cart: {
                         items: [],
                     },

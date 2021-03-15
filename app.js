2a1114e046867ffe717604b3d85f486d2a3694d7
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-03-15 21:08:25 +0200

    Adding error handling page

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ const errorController = require("./controllers/error");
 const User = require("./models/user");
 
 const MONGODB_URI =
-    "mongodb+srv://Mohamed9Yasser:qxRcvxU5eCXPfi2H@cluster0.vtzp8.mongodb.net/Shop";
-
+    "mongodb+srv://Mohamed9Yasser:qxRcvxU5eCXPfi2H@cluster0.vtzp8.mongodb.net/Shop?retryWrites=true&w=majority";
 const app = express();
 const store = new MongoDBStore({
     uri: MONGODB_URI,
@@ -27,7 +26,6 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
-const { error } = require("console");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -39,11 +37,17 @@ app.use(
         store: store,
     })
 );
-
 app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
+
+app.use((req, res, next) => {
+    // throw new Error('Sync Dummy');
     if (!req.session.user) {
         return next();
     }
@@ -56,13 +60,8 @@ app.use((req, res, next) => {
             next();
         })
         .catch((err) => {
-            throw new Error(err);
+            next(new Error(err));
         });
-});
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
 });
 
 app.use("/admin", adminRoutes);
@@ -74,7 +73,13 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-    res.redirect("/500");
+    // res.status(error.httpStatusCode).render(...);
+    // res.redirect('/500');
+    res.status(500).render("500", {
+        pageTitle: "Error!",
+        path: "/500",
+        isAuthenticated: req.session.isLoggedIn,
+    });
 });
 
 mongoose
